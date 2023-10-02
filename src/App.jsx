39abc88f4767504c6b6490394cb942b6d4b17722
akabishau <aleksey.kabishau@icloud.com>
2023-10-02 10:26:39 -0400

    clean up boilerplate template code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import './App.css'
 
 const todoList = [
   { id: 1, title: 'Learn about CRA way to initiate React app' },
   { id: 2, title: 'Learn about Vite'},
-  { id: 3, title: 'Create app using CRA' },
-  { id: 4, title: 'Create app using Vite' },
-  { id: 4, title: 'Stick with Vite version' },
+  { id: 3, title: 'Try creating app using CRA' },
+  { id: 4, title: 'Try creating app using Vite' },
+  { id: 4, title: 'Keep developing using better option' },
 ]
 
 
@@ -17,7 +13,7 @@ function App() {
   return (
       <div>
 
-        <h1>My TODO List</h1>
+        <h1>My List</h1>
         <ul>
           { todoList.map(item => <li key={item.id}>{item.title}</li>) }
         </ul>
