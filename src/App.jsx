54e6900ee1a450efb7aa45d1f6a3991310758382
akabishau <aleksey.kabishau@icloud.com>
2023-10-09 13:48:16 -0400

    add todo form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import TodoList from './TodoList'
-
+import AddTodoForm from './AddTodoForm'
 
 function App() {
   return (
       <div>
         <h1>My List</h1>
+        <AddTodoForm/>
         <TodoList/>
       </div>
   )
