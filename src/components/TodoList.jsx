3fbbf4b544ff61ab4306e423d1477117eeff5f7f
Aleksey K <aleksey.kabishau@icloud.com>
2024-02-06 19:29:08 -0500

    Revert "Lesson 3.1: CSS, Styled Components, SVG"

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,31 @@
-import styles from '../styles/TodoListItem.module.css';
+import styled from 'styled-components';
 import IconButton from './IconButton';
 
 export default function TodoList({ todoList, onRemoveTodo }) {
   return (
     <ul>
       {todoList.map(item => (
-        <TodoListItem key={item.id} item={item} onRemoveTodo={onRemoveTodo} />
+        <Item key={item.id}>
+          <span>{item.title}</span>
+          <IconButton variant="remove" onClick={() => onRemoveTodo(item.id)} />
+        </Item>
       ))}
     </ul>
   );
 }
 
-function TodoListItem({ item, onRemoveTodo }) {
-  return (
-    <li className={styles.item}>
-      <span>{item.title}</span>
-      <IconButton variant="remove" onClick={() => onRemoveTodo(item.id)} />
-    </li>
-  );
-}
+const Item = styled.li`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 10px;
+  background-color: #3a3a3a;
+  color: #ffffff;
+  padding: 10px 15px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #555555;
+  }
+`;
