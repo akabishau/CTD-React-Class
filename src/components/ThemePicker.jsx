a834c9dcebc85327dcb9e3ab9bbc5e141e5da045
akabishau <aleksey.kabishau@icloud.com>
2024-10-24 12:21:33 -0400

    add react-selector library to improve view appearance

--- src/components/ThemePicker.jsx
+++ src/components/ThemePicker.jsx
@@ -1,29 +1,89 @@
+import Select from 'react-select';
 import { useTheme } from '../hooks/useTheme';
 import { THEMES } from '../constants/uiConfig';
 
-function getThemeOptions() {
-  return Object.keys(THEMES).map(option => ({
-    value: option,
-    label: THEMES[option].selector
-  }));
-}
-
-export default function ThemeSelector() {
+export default function ThemePicker() {
   const { theme, setTheme } = useTheme();
-  const themeOptions = getThemeOptions();
+
+  const themeOptions = Object.keys(THEMES).map(key => ({
+    value: key,
+    label: THEMES[key].name,
+    icon: THEMES[key].icon
+  }));
+
+  const handleChange = selectedOption => {
+    setTheme(selectedOption.value); // Update the theme when an option is selected
+  };
+
+  // TODO: review and refactor styles
+  // custom styles for the dropdown
+  const customStyles = {
+    // main container for selected value and dropdown indicator
+    control: (provided, state) => ({
+      ...provided,
+      backgroundColor: '#3a3a3a',
+      border: '1px solid #555555',
+      boxShadow: state.isFocused ? 'none' : 'none', // remove blue outline
+      color: '#ffffff',
+      '&:hover': {
+        borderColor: '#ffffff'
+      }
+    }),
+
+    // selected option value
+    singleValue: provided => ({
+      ...provided,
+      display: 'flex',
+      alignItems: 'center',
+      color: '#ffffff'
+    }),
+
+    // dropdown indicator
+    dropdownIndicator: provided => ({
+      ...provided
+    }),
+
+    // container for all options in the dropdown
+    menu: provided => ({
+      ...provided,
+      backgroundColor: '#3a3a3a'
+    }),
+
+    // container for the list of options - max height and scrolling
+    menuList: provided => ({
+      ...provided
+    }),
+
+    // individual option item
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? '#555555' : '#3a3a3a',
+      color: '#ffffff',
+      display: 'flex',
+      alignItems: 'center',
+      padding: '10px'
+    })
+  };
+
+  // configure to include icon and lable to the dropdown
+  const formatOptionLabel = ({ label, icon }) => (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={icon} alt={label} style={{ width: 20, marginRight: 10 }} />
+      {label}
+    </div>
+  );
 
   return (
-    <select
-      value={theme}
-      onChange={event => {
-        setTheme(event.target.value);
-      }}
-    >
-      {Object.keys(THEMES).map(option => (
-        <option key={option} value={option}>
-          {THEMES[option].selector}
-        </option>
-      ))}
-    </select>
+    <Select
+      value={themeOptions.find(option => option.value === theme)}
+      onChange={handleChange}
+      options={themeOptions}
+      styles={customStyles}
+      formatOptionLabel={formatOptionLabel}
+      isSearchable={false} // optional/default is false
+      placeholder="" // optional, not needed in this case
+    />
   );
 }
+
+// TODO: consider building default app dropdown style and inheriting from it
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,28 +1,15 @@
 import DefaultLayout from '../layouts/DefaultLayout';
 import ThemePicker from '../components/ThemePicker';
-import { THEMES } from '../constants/uiConfig';
 import { H1 } from '../styles/TextStyles';
 import { useTheme } from '../hooks/useTheme';
 
 export default function SettingsPage() {
-  const { themeConfig, theme, setTheme } = useTheme();
+  const { themeConfig } = useTheme();
 
   return (
     <DefaultLayout>
       <H1>{themeConfig.navigation.settings.heading}</H1>
-      {/* TODO: Create ThemeSelector component */}
-      <select
-        value={theme}
-        onChange={event => {
-          setTheme(event.target.value);
-        }}
-      >
-        {Object.keys(THEMES).map(option => (
-          <option key={option} value={option}>
-            {THEMES[option].selector}
-          </option>
-        ))}
-      </select>
+      <ThemePicker />
     </DefaultLayout>
   );
 }
